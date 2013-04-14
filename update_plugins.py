349a9f5cdacf73c526e80ee3bff58daabf09eaf7
amix <amix@amix.dk>
2013-04-14 12:55:47 -0300

    Added a new maping to update_plugins.py

--- update_plugins.py
+++ update_plugins.py
@@ -30,6 +30,7 @@ vim-snipmate https://github.com/garbas/vim-snipmate
 vim-snippets https://github.com/honza/vim-snippets
 vim-surround https://github.com/tpope/vim-surround
 vim-expand-region https://github.com/terryma/vim-expand-region
+vim-multiple-cursors https://github.com/terryma/vim-multiple-cursors
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
