2ef74326f064c17f2309e1e2b5930e67c0fefc3c
amix <amix@amix.dk>
2017-07-16 14:33:43 +0200

    Fixed https://github.com/tpope/tpope-vim-abolish

--- update_plugins.py
+++ update_plugins.py
@@ -51,6 +51,7 @@ vim-flake8 https://github.com/nvie/vim-flake8
 vim-pug https://github.com/digitaltoad/vim-pug
 vim-yankstack https://github.com/maxbrunsfeld/vim-yankstack
 lightline.vim https://github.com/itchyny/lightline.vim
+tpope-vim-abolish https://github.com/tpope/tpope-vim-abolish
 """.strip()
 
 GITHUB_ZIP = '%s/archive/master.zip'
