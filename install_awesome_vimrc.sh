d5797265cb912c300844a32bce16fe4284f85d6a
Chris Morris <cmorris@pagerduty.com>
2019-04-09 10:53:15 -0400

    add brew, macvim and vim install to the setup script
    [master]

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -16,3 +16,21 @@ catch
 endtry' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
+
+# check for mvim installation
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "Checking for brew ....."
+  which -s brew
+  if [[ $? != 0 ]] ; then
+    echo "Brew not found. Starting install now ...."
+    # Install Homebrew
+    # https://github.com/mxcl/homebrew/wiki/installation
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+    echo "Brew install complete!"
+  else
+    echo "Brew found. Running update...."
+    brew update
+  fi
+  echo "Installing macvim and vim"
+  brew install macvim vim
+fi
