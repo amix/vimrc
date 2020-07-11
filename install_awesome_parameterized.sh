04988ed839875643a007d0a47a0893833988de4a
Quentin Meeus <qmeeus@gmail.com>
2020-07-11 05:26:23 +0200

    fix typo when installing for specific users (#578)
    line 34 had `~/$user` which evaluates to `/home/user1/user1`. Now changed into `~$user` as above

--- install_awesome_parameterized.sh
+++ install_awesome_parameterized.sh
@@ -31,7 +31,7 @@ else
     SELECTED_USERS=${@:2}
     echo "Selected users: $SELECTED_USERS"
     for user in $SELECTED_USERS; do
-        homepath=$(eval echo "~/$user")
+        homepath=$(eval echo "~$user")
         IFS=''
         echo $VIMRC > ${homepath}/.vimrc
         unset IFS
