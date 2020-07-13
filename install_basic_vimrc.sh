5a4462eba9066941be8178cc43027c94b6b61c78
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-07-13 19:45:58 +0530

    Save old vim configs before writing into ~/.vimrc.
    If user has some vimrc already present then it will be saved as
    $HOME/.oldvimrc and then new vimrc will be created.

--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+# Save previous vimrc.
+if [ -f $HOME/.vimrc ]; then
+    mv ~/.vimrc ~/.oldvimrc
+    echo "Saved previous vimrc as $HOME/.oldvimrc"
+fi
 cd ~/.vim_runtime
 cat ~/.vim_runtime/vimrcs/basic.vim > ~/.vimrc
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
