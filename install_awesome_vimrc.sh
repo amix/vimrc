012bdfe0d6fe9aa399b8033876bf396dd8ac381b
Revc <x15967311210@gmail.com>
2021-02-18 13:03:29 +0800

    add lines for loading eden_configs.vim firstly
    Add lines for loading eden_configs.vim firstly.

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -5,6 +5,11 @@ cd ~/.vim_runtime
 
 echo 'set runtimepath+=~/.vim_runtime
 
+try
+source ~/.vim_runtime/eden_configs.vim
+catch
+endtry
+
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
 source ~/.vim_runtime/vimrcs/plugins_config.vim
